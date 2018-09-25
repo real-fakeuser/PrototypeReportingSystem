fa3d2041b39ac3d6ea6ae0e92b513b82fbf8024e
realfakeuser <tim.niewerth@tobit.software>
2018-09-25 17:28:55 +0200

    no important changes

--- src/containers/CreateReportContainer.jsx
+++ src/containers/CreateReportContainer.jsx
@@ -5,22 +5,62 @@ import { fromJS } from 'immutable';
 import { PersonFinder } from 'chayns-components';
 import { addReport } from '../actions/reportForm';
 import { Button, ChooseButton } from 'chayns-components';
+import { FileUpload } from 'chayns-components';
+import './CreateReportContainer.scss';
+import 'chayns-components/lib/react-chayns-upload/index.css';
+import { SelectList, SelectListItem } from 'chayns-components';
+import 'chayns-components/lib/react-chayns-selectlist/index.css';
 import { Input } from 'chayns-components';
+import { ContextMenu } from 'chayns-components';
+import 'chayns-components/lib/react-chayns-contextmenu/index.css';
+import { ScrollView } from 'chayns-components';
+import 'chayns-components/lib/react-chayns-scrollview/index.css';
+
 
 const ReportFormWrapper = ({ addReport }) => (
+    <div>
 
-<Input
-placeholder="Looking for 3 lowercase e's"
-onInput={ (text) => { console.log('There is some new input. Check the new text: ', text); }}
-onBlur={ (text) => { console.log('The input losted its focus, the final input is: ', text); }}
-onChange={(result) => {
+        <h2>Erstelle Hier Deinen Fehler-Report</h2>
+        <FileUpload
+            multiple={false}
+            type="image"
+            onChange={(files, validFiles) => {
+                console.log(`You have selected ${files.length} files of which ${validFiles.length} are valid`);
+            }}
+        />
+        <Input
 
-    console.log(result);
+            placeholder="Worum geht es?"
+            onInput={(text) => { console.log('There is some new input. Check the new text: ', text); }}
+            onBlur={(text) => { console.log('The input losted its focus, the final input is: ', text); }}
+            onChange={(result) => {
 
-}}
+                console.log(result);
 
-/>
+            }}
+        />
+        <div>
+            <a>Standort: </a>
+            <select>
+                <option value="volvo">Volvo</option>
+                <option value="saab">Saab</option>
+                <option value="mercedes">Mercedes</option>
+                <option value="audi">Audi</option>
+            </select>
+            <div className='input_wrapper'>
+            <div className='select_wrapper'>
+            <select><option>Tag</option><option>Woche</option><option>Monat</option><option>Quartal</option></select>
+            </div>
+            </div>
 
+            <div className='select_wrapper'><select><option>Tag</option><option>Woche</option><option>Monat</option><option>Quartal</option></select></div>
+
+        </div>
+
+
+
+
+    </div>
 );
 
 ReportFormWrapper.propTypes = {
