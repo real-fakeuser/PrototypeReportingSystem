7856c0cae5af58e149035b25b3747bf9fae2518e
realfakeuser <tim.niewerth@tobit.software>
2018-09-28 17:21:38 +0200

    some changes

--- src/components/ListItem/listItem.jsx
+++ src/components/ListItem/listItem.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './listItem.scss'
+import { SelectButton } from 'chayns-components';
+import { Accordion } from 'chayns-components';
+import HistoryListItem from '../HistoryList/listItem';
 
 const propTypes = {
     titleString: PropTypes.string,
@@ -45,16 +48,18 @@ export default class ListItem extends React.Component {
     render = () => {
         return (
             <div
-                onClick={() => {
-                    if (this.itemHead.current.classList.contains("ListItem--accordion--open"))
-                        this.itemHead.current.classList.remove("ListItem--accordion--open");
-                    else
-                        this.itemHead.current.classList.add("ListItem--accordion--open");
-                }}
                 ref={this.itemHead}
                 className="ListItem ListItem--accordion"
             >
-                <div className="ListItem__head">
+                <div className="ListItem__head"
+                    onClick={() => {
+                        if (this.itemHead.current.classList.contains("ListItem--accordion--open"))
+                            this.itemHead.current.classList.remove("ListItem--accordion--open");
+                        else
+                            this.itemHead.current.classList.add("ListItem--accordion--open");
+                    }}
+
+                >
                     <div className="ListItem__Arrow">
                         <i className="fa fa-chevron-right" />
                     </div>
@@ -67,11 +72,135 @@ export default class ListItem extends React.Component {
                 <div className="ListItem__body">
                     <div className="ListItem__Content">
 
-                        <div className='flexR FormRow'>
 
-                            {
-                                this.props.accordionBody
-                            }
+
+
+
+                        <div>
+                            <div className='flexR FormRow'>
+                                <div className="flex__left">Ersteller</div>
+                                <div>
+                                    <a>Max Mustermann</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className='flexR FormRow'>
+                                <div className="flex__left">Abteilung</div>
+                                <div>
+                                    <SelectButton
+                                        label="Ändern"
+                                        list={[{
+                                            id: '0', //this is the listKey
+                                            name: 'Facility Management', //this ist the listValue (shown value inside the dialog)
+                                            price: '4.00' //additional data that will be returned after selection
+                                        }, {
+                                            id: '1',
+                                            name: 'Administration',
+                                            price: '4.50'
+                                        }, {
+                                            id: '2',
+                                            name: 'Personalmanagement',
+                                            price: '4.50'
+                                        }, {
+                                            id: '3',
+                                            name: 'Küche',
+                                            price: '5.00'
+                                        }]}
+                                        onSelect={(value) => { console.log(value) }}
+                                        listKey="id"
+                                        listValue="name"
+                                    />
+                                </div>
+                            </div>
+                            <div className='title '>
+                                <div className='titleimage2'>
+                                </div>
+                            </div>
+                            <div>
+                                Hier steht ein Beschriebungstext...Hier steht ein Beschriebungstext...Hier steht ein Beschriebungstext...Hier steht ein Beschriebungstext...Hier steht ein Beschriebungstext...Hier steht ein Beschriebungstext...
+                            </div>
+
+
+
+
+                            <Accordion head='Verlauf'
+                                style={{ 'marginBottom': '5px', 'borderBottom': 'none'}}
+                                defaultOpened={false}>
+                                <div className="accordion__content" style={{ 'marginLeft': '0', 'paddingLeft': '9px', 'paddingTop': '10px', 'paddingBottom': '9px' }}>
+                                    <div>
+                                        <HistoryListItem
+                                            titleString="Günter Neu"
+                                            description="Übernahm den Auftrag"
+                                            badgeText="Vor 23 Min"
+                                        />
+                                        <HistoryListItem
+                                            titleString="Gerd Albers"
+                                            description="Wies den Auftrag der Gruppe Junior.Team zu"
+                                            badgeText="Vor 70 Min"
+                                        />
+                                        <HistoryListItem
+                                            titleString="Gerd Albers"
+                                            description="Übernahm den Report"
+                                            badgeText="Vor 72 Min"
+                                        />
+                                        <HistoryListItem
+                                            titleString="Max Mustermann"
+                                            description="Erstellte Report"
+                                            badgeText="Vor 82 Min"
+                                        />
+
+
+
+
+
+                                    </div>
+                                </div>
+                            </Accordion>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
                         </div>
                     </div>
